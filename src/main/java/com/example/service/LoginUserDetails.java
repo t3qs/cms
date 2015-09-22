45ff205ca738c1bd8328f07e1a80dadfb103eebc
kyun <yeopoong@gmail.com>
2015-09-22 15:52:59 +0900

    lombok remove

--- src/main/java/com/example/service/LoginUserDetails.java
+++ src/main/java/com/example/service/LoginUserDetails.java
@@ -1,15 +1,20 @@
 package com.example.service;
 
-import com.example.domain.User;
-import lombok.Data;
 import org.springframework.security.core.authority.AuthorityUtils;
 
-@Data
+import com.example.domain.User;
+
 public class LoginUserDetails extends org.springframework.security.core.userdetails.User {
-    private final User user;
+	private static final long serialVersionUID = 1L;
+
+	private final User user;
 
     public LoginUserDetails(User user) {
         super(user.getUsername(), user.getEncodedPassword(), AuthorityUtils.createAuthorityList("ROLE_USER"));
         this.user = user;
     }
+
+	public User getUser() {
+		return user;
+	}
 }
